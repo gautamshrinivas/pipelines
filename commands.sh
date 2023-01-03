3ca17b5de93794c346dba7cb28aed2c8b1258b24
gautam <gautamrck76@gmail.com>
2023-01-03 11:25:18 +0530

    fresh chnages

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                
-docker image build -t nopimage:1.0 -f ./nopcommerce-dockerfile .
+docker image build -t nopimage:1.1 -f ./nopcommerce-dockerfile .
 docker images
-docker container run -d --name nopcontainer -P nopimage:1.0
+docker container run -d --name nopcontainer -P nopimage:1.1
 docker container ls -a
-docker image tag nopimage:1.0 gautamshrinivas/nopimage:1.0
-docker push gautamshrinivas/nopimage:1.0
+docker image tag nopimage:1.1 gautamshrinivas/nopimage:1.1
+docker push gautamshrinivas/nopimage:1.1
